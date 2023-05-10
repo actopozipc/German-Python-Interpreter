de811becd66ccbd49baf8b778a58a0d2d0b316f6
actopozipc <48481041+actopozipc@users.noreply.github.com>
2023-05-10 20:29:47 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,35 @@
+importiere numpy als np
+von numpy importiere Wurzel
+klasse Person:
+    def __init__(selbst, name, alter):
+        selbst.name = name
+        selbst.alter = alter
+    
+    def gruesse(selbst):
+        drucke(f'Hallo, ich bin {selbst.name} und {selbst.alter} Jahre alt.')
+
+p = Person('Max', 25)
+p.gruesse()
+
+zahl = 10
+solange zahl > 0:
+    wenn zahl % 2 == 0:
+        drucke(f'{zahl} ist gerade.')
+    sonst:
+        drucke(f'{zahl} ist ungerade.')
+    zahl -= 1
+
+liste = [1, 2, 3, 4, 5, 7, 8]
+für element in liste:
+    wenn element == 1:
+        fortsetze
+    andernfalls element == 7:
+        breche
+    drucke(f'Die Wurzel von {element} ist {np.Wurzel(element)}.')
+    
+versuche:
+    1 / 0
+ausnahme Nullteilungsfehler als e:
+    drucke(f'Division durch Null: {e}.')
+finalisierend:
+    drucke('Das Programm ist fertig')
